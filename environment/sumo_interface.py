43219b44eb703864b4b7e046d9c206ef9442f139
Ferdinand Tonby-Strandborg <ferdinand.tonby.strandborg@gmail.com>
2025-10-27 13:42:22 +0800

    Added range of step calculation

--- environment/sumo_interface.py
+++ environment/sumo_interface.py
@@ -93,6 +93,7 @@ SumoInterface(fname, *, [gui], [seed], ...)
 import os
 import sys
 import heapq
+import math
 from itertools import permutations
 
 import numpy as np
@@ -138,6 +139,7 @@ class SumoInterface:
 
     def _init(self):
         self._ytime    = 5
+        self._steptime = 1
         self._carindex = 0
 
         # 1) Get Lanes
@@ -318,7 +320,7 @@ class SumoInterface:
         self._sim.close()
         self._start()
 
-    def step(self, seconds=1):
+    def step(self):
         """Go to the next "environment step"."""
         self._pre_update()
         # Get new traffic light if it has changed
@@ -332,7 +334,7 @@ class SumoInterface:
             # Then set new green/red lights
             self._sim.trafficlight.setRedYellowGreenState(NODE, newlight)
         # Step for desired "evironment step duration"
-        for _ in range(int(seconds / self._deltat)):
+        for _ in range(int(self._steptime / self._deltat)):
             self._sim.simulationStep()
             self._partial_update()
         # Get elapsed time in sim
@@ -442,6 +444,19 @@ class SumoInterface:
     def get_cars_added(self):
         """Check how many cars have been added to the simulation."""
         return self._carindex
+
+    ### --- TO GET RANGE OF STEPS --------------------------------------
+    def min_step_t(self):
+        """Note - based off assumption of deltat := 0.1"""
+        return self._steptime
+    
+    def max_step_t(self):
+        """Note - based off assumption of deltat := 0.1"""
+        return self._ytime + self._steptime
+
+    def steps_needed(self, time):
+        """Returns (lower limit, upper limit)"""
+        return (math.floor(time / self.max_step_t()), math.ceil(time / self.min_step_t()))
 
     ### --- FOR TESTING --- --------------------------------------------
     def visualize(self):
