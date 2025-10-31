f8eb7b4691cbab1cd2ee3ba79e44b9e724d7d8b5
Ferdinand O. M Tonby-Strandborg <ferdinand.tonby.strandborg@gmail.com>
2025-10-31 18:46:47 +0800

    Suppressed if PIL unavailable

--- environment/giffer.py
+++ environment/giffer.py
@@ -10,10 +10,15 @@ for i in range(100):
     gif.update()
 gif.save("Result.gif")
 """
-# PIP INSTALL PILLOW
-from PIL import Image, ImageDraw
-from math import floor
-import numpy as np
+try:
+    from PIL import Image, ImageDraw
+    from math import floor
+    import numpy as np
+
+    USING_PIL = True
+except ModuleNotFoundError:
+    USING_PIL = False
+
 
 SCALE = 4
 # T_PER_FRAME = 8.0
@@ -28,123 +33,141 @@ def _scale(pt, pt1=None):
     x, y = pt if pt1 is None else (pt, pt1)
     return (x - 100) * SCALE, (y - 100) * SCALE
 
+### --- Main Class --- -----------------------------------------------
 class SumoGif:
+    """This class should be passed to the SumoInterface class, and will on each step generate a frame.
+
+    It assumes that PIL is available, and in the case that it is not available for whatever reason, each function will be empty.
+    
+    This behaviour is because the GIF is optional, and I don't want to have submission issues due to this...
+    """
     def __init__(self, sim, name, *, cars=False):
-        self.sim       = sim
-        self.draw_cars = cars
-        self.name      = name
-        
-        # Sensor positions
-        self.sp = self.sim.get_sensor_positions()
-        # Light positions
-        self.lp = [(l[2], l[3]) for l in self.sim.get_lane_midpoints('in')]
-        # Text (queue length) positions
-        self.tp = [perp(proj(l[2:], l[:2], 60), l[:2], 20) for l in self.sim.get_lane_midpoints('in')]
+        if USING_PIL:
+            self.sim       = sim
+            self.draw_cars = cars
+            self.name      = name
+            
+            # Sensor positions
+            self.sp = self.sim.get_sensor_positions()
+            # Light positions
+            self.lp = [(l[2], l[3]) for l in self.sim.get_lane_midpoints('in')]
+            # Text (queue length) positions
+            self.tp = [perp(proj(l[2:], l[:2], 60), l[:2], 20) for l in self.sim.get_lane_midpoints('in')]
 
-        self.frames = []
+            self.frames = []
 
-        self._gen_background()
+            self._gen_background()
 
     def save(self):
-        if not self.frames:
-            return
-        self.frames[0].save(self.name, save_all=True, append_images=self.frames[1:], optimize=False, duration=1000/FPS)
-        print(comment("Saved gif to", self.name))
+        if USING_PIL:
+            if not self.frames:
+                return
+            self.frames[0].save(self.name, save_all=True, append_images=self.frames[1:], optimize=False, duration=1000/FPS)
+            print(comment("Saved gif to", self.name))
 
     def _gen_background(self):
-        # "SETTINGS"
-        self.w, self.h = 200*SCALE, 200*SCALE
-        
-        self.bg = Image.new("RGBA", (self.w, self.h), 'white')
-        self.bgdraw = ImageDraw.Draw(self.bg)
-        
-        # 1) Draw intersection
-        intersection = [_scale(x, y) for x, y in self.sim.get_intersection_shape()]
-        self.bgdraw.polygon(intersection, fill="#ddddddff", outline="#dddddd")
-        
-        # 2) Draw lanes
-        lanes = [(_scale(x0, y0), _scale(x1, y1)) for x0, y0, x1, y1 in self.sim.get_lane_midpoints('in')]
-        for l in lanes:
-            self.bgdraw.line(l, fill="#ddddddff", width=0)
-        lanes = [(_scale(x0, y0), _scale(x1, y1)) for x0, y0, x1, y1 in self.sim.get_lane_midpoints('out')]
-        for l in lanes:
-            self.bgdraw.line(l, fill="#ddddddff", width=0)
+        if USING_PIL:
+            # "SETTINGS"
+            self.w, self.h = 200*SCALE, 200*SCALE
+            
+            self.bg = Image.new("RGBA", (self.w, self.h), 'white')
+            self.bgdraw = ImageDraw.Draw(self.bg)
+            
+            # 1) Draw intersection
+            intersection = [_scale(x, y) for x, y in self.sim.get_intersection_shape()]
+            self.bgdraw.polygon(intersection, fill="#ddddddff", outline="#dddddd")
+            
+            # 2) Draw lanes
+            lanes = [(_scale(x0, y0), _scale(x1, y1)) for x0, y0, x1, y1 in self.sim.get_lane_midpoints('in')]
+            for l in lanes:
+                self.bgdraw.line(l, fill="#ddddddff", width=0)
+            lanes = [(_scale(x0, y0), _scale(x1, y1)) for x0, y0, x1, y1 in self.sim.get_lane_midpoints('out')]
+            for l in lanes:
+                self.bgdraw.line(l, fill="#ddddddff", width=0)
 
-        # 3) Draw Sensors
-        for s in self.sp:
-            for x, y in s:
-                pts = [_scale(x-LW, y-LW), _scale(x+LW, y+LW)]
-                # self.sdraw.ellipse(pts, fill="#dddddd88", outline="#ddddddFF")
-                self.bgdraw.ellipse(pts, fill="#FFFFFFFF", outline="#ddddddFF")
+            # 3) Draw Sensors
+            for s in self.sp:
+                for x, y in s:
+                    pts = [_scale(x-LW, y-LW), _scale(x+LW, y+LW)]
+                    # self.sdraw.ellipse(pts, fill="#dddddd88", outline="#ddddddFF")
+                    self.bgdraw.ellipse(pts, fill="#FFFFFFFF", outline="#ddddddFF")
 
     def car_frame(self):
-        frame = Image.new("RGBA", self.bg.size, (255, 255, 255, 0))
-        fdraw = ImageDraw.Draw(frame)
-        for x, y, a, l, w in self.sim.get_car_midpoints():
-            pts = [_scale(x-w, y-w), _scale(x+w, y+w)]
-            fdraw.ellipse(pts, fill="#00000066", outline="#000000FF")
-        return frame
+        if USING_PIL:
+            frame = Image.new("RGBA", self.bg.size, (255, 255, 255, 0))
+            fdraw = ImageDraw.Draw(frame)
+            for x, y, a, l, w in self.sim.get_car_midpoints():
+                pts = [_scale(x-w, y-w), _scale(x+w, y+w)]
+                fdraw.ellipse(pts, fill="#00000066", outline="#000000FF")
+            return frame
 
     def sensor_frame(self):
-        frame = Image.new("RGBA", self.bg.size, (255, 255, 255, 0))
-        fdraw = ImageDraw.Draw(frame)
-        for lp, lt in zip(self.sp, self.sim.get_occupied_time()):
-            for (x, y), t in zip(lp, lt):
-                if t > 0:
-                    border = "#8844DDEE"
-                else:
-                    continue
-                if t > MAX_TIME:
-                    fill = "#000000FF"
-                else:
-                    v = int(t // MT)
-                    v = v if v <= 15 else 15
-                    fill = "#002255" + hex(v).replace("0x", "") * 2
-                pts = [_scale(x-LW, y-LW), _scale(x+LW, y+LW)]
-                fdraw.ellipse(pts, fill=fill, outline=border)
-        return frame
+        if USING_PIL:
+            frame = Image.new("RGBA", self.bg.size, (255, 255, 255, 0))
+            fdraw = ImageDraw.Draw(frame)
+            for lp, lt in zip(self.sp, self.sim.get_occupied_time()):
+                for (x, y), t in zip(lp, lt):
+                    if t > 0:
+                        border = "#8844DDEE"
+                    else:
+                        continue
+                    if t > MAX_TIME:
+                        fill = "#000000FF"
+                    else:
+                        v = int(t // MT)
+                        v = v if v <= 15 else 15
+                        fill = "#002255" + hex(v).replace("0x", "") * 2
+                    pts = [_scale(x-LW, y-LW), _scale(x+LW, y+LW)]
+                    fdraw.ellipse(pts, fill=fill, outline=border)
+            return frame
     
     def light_frame(self):
-        frame = Image.new("RGBA", self.bg.size, (255, 255, 255, 0))
-        fdraw = ImageDraw.Draw(frame)
-        for (x, y), g in zip(self.lp, self.sim.get_lights()):
-            if g:
-                fill = "#00FF00FF"
-            else:
-                fill = "#FF0000FF"
-            pts = [_scale(x-LR, y-LR), _scale(x+LR, y+LR)]
-            fdraw.ellipse(pts, fill=fill, outline=fill)
-        return frame
+        if USING_PIL:
+            frame = Image.new("RGBA", self.bg.size, (255, 255, 255, 0))
+            fdraw = ImageDraw.Draw(frame)
+            for (x, y), g in zip(self.lp, self.sim.get_lights()):
+                if g:
+                    fill = "#00FF00FF"
+                else:
+                    fill = "#FF0000FF"
+                pts = [_scale(x-LR, y-LR), _scale(x+LR, y+LR)]
+                fdraw.ellipse(pts, fill=fill, outline=fill)
+            return frame
     
     def queue_len_frame(self):
-        frame = Image.new("RGBA", self.bg.size, (255, 255, 255, 0))
-        fdraw = ImageDraw.Draw(frame)
-        for (x, y), q in zip(self.tp, self.sim.get_queue_length()):
-            fdraw.text(_scale(x, y), str(q), fill="#00BBBBFF")
-            # fdraw.ellipse(pts, str(q), fill="#00BBBBFF")
-        return frame
+        if USING_PIL:
+            frame = Image.new("RGBA", self.bg.size, (255, 255, 255, 0))
+            fdraw = ImageDraw.Draw(frame)
+            for (x, y), q in zip(self.tp, self.sim.get_queue_length()):
+                fdraw.text(_scale(x, y), str(q), fill="#00BBBBFF")
+                # fdraw.ellipse(pts, str(q), fill="#00BBBBFF")
+            return frame
 
     def time_frame(self):
-        frame = Image.new("RGBA", self.bg.size, (255, 255, 255, 0))
-        fdraw = ImageDraw.Draw(frame)
-        fdraw.text((self.bg.size[0] * 0.9, self.bg.size[1] * 0.1),
-                    "Time: " + str(self.sim.get_time()),
-                    fill="#000000FF")
-        return frame
+        if USING_PIL:
+            frame = Image.new("RGBA", self.bg.size, (255, 255, 255, 0))
+            fdraw = ImageDraw.Draw(frame)
+            fdraw.text((self.bg.size[0] * 0.9, self.bg.size[1] * 0.1),
+                        "Time: " + str(self.sim.get_time()),
+                        fill="#000000FF")
+            return frame
 
     def compose(self):
-        frame = self.bg.copy()
-        if self.draw_cars:
-            frame = Image.alpha_composite(frame, self.car_frame())
-        frame = Image.alpha_composite(frame, self.sensor_frame())
-        frame = Image.alpha_composite(frame, self.light_frame())
-        frame = Image.alpha_composite(frame, self.queue_len_frame())
-        frame = Image.alpha_composite(frame, self.time_frame())
-        return frame
+        if USING_PIL:
+            frame = self.bg.copy()
+            if self.draw_cars:
+                frame = Image.alpha_composite(frame, self.car_frame())
+            frame = Image.alpha_composite(frame, self.sensor_frame())
+            frame = Image.alpha_composite(frame, self.light_frame())
+            frame = Image.alpha_composite(frame, self.queue_len_frame())
+            frame = Image.alpha_composite(frame, self.time_frame())
+            return frame
 
     def update_buffer(self, highlight=False):
-        self.frames.append(self.compose())
+        if USING_PIL:
+            self.frames.append(self.compose())
 
+### --- Utilities --- -----------------------------------------------------------------------------
 def comment(*msg):
     return "\033[2m\033[32m" + " ".join([str(m) for m in msg]) + "\033[0m"
 
@@ -171,16 +194,16 @@ def ptc(p, p1=None):
     r, a = p if p1 is None else (p, p1)
     return r * np.cos(np.deg2rad(a)), r * np.sin(np.deg2rad(a))  
 
+### --- Test Code --- -------------------------------------------------------------------------------
 if __name__ == "__main__":
     from sumo_interface import SumoInterface
     from time import time, sleep
 
-    sim = SumoInterface("map_1")
+    sim = SumoInterface("map_1", gif="temp.gif")
     sim.set_car_prob([0.2] * 12)
-    gif = SumoGif(sim, "temp.gif", True)
     for i in range(100):
         sim.step()
-        gif.update_buffer()
-    gif.save()    
 
-    sleep(0.2)
+    del sim
+
+    sleep(5) # Just in case save didn't run quickly enough
