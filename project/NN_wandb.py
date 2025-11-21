1aee3a1433d1a6908ac06908529763b883a731b1
Ferdinand Tonby-Strandborg <ferdinand.tonby.strandborg@gmail.com>
2025-11-21 15:15:05 +0800

    Cleanup

--- project/NN_wandb.py
+++ project/NN_wandb.py
@@ -1,73 +1,28 @@
+"""
+To run (from projects dir:)
+python NN_wandb.py
+
+To run (from root dir:)
+python project/NN_wandb.py
+
+To use log your test:
+python environment/NN_wandb.py --wandb-name <testname>
+ 
+If you dont want to use wandb:
+python environment/NN_wandb.py --no-wandb
+"""
 import numpy as np
 import torch
-import torch.nn as nn
-import torch.optim as optim
-from collections import deque
+import wandb
+
+from datetime import datetime
 import random
+import os
 
 from trafficlightgymsumo_NN_wandb import TrafficGym
 from giffer import SumoGif
 from nn import NN, DuelingNN
-
-import argparse
-import wandb
-import os
-from datetime import datetime
-
-
-reasonable_actions = [
-3,  # North Left+Forward
-4,  # North Right Only
-7,  # North All
-24,  # East Left+Forward
-32,  # East Right Only
-56,  # East All
-192,  # South Left+Forward
-195,  # North Left+Forward + South Left+Forward
-196,  # North Right + South Left+Forward
-199,  # North All + South Left+Forward
-256,  # South Right Only
-259,  # North Left+Forward + South Right
-260,  # North Right + South Right
-263,  # North All + South Right
-448,  # South All
-451,  # North Left+Forward + South All
-452,  # North Right + South All
-455,  # North All + South All
-1536,  # West Left+Forward
-1560,  # East Left+Forward + West Left+Forward
-1568,  # East Right + West Left+Forward
-1592,  # East All + West Left+Forward
-2048,  # West Right Only
-2072,  # East Left+Forward + West Right
-2080,  # East Right + West Right
-2104,  # East All + West Right
-3584,  # West All
-3608,  # East Left+Forward + West All
-3616,  # East Right + West All
-3640  # East All + West All
-]
-
-
-
-
-
-class ReplayBuffer:
-    """Experience replay buffer for storing transitions"""
-    def __init__(self, capacity):
-        self.buffer = deque(maxlen=capacity)
-    
-    def push(self, state, action, reward, next_state, done):
-        self.buffer.append((state, action, reward, next_state, done))
-    
-    def sample(self, batch_size):
-        batch = random.sample(self.buffer, batch_size)
-        states, actions, rewards, next_states, dones = zip(*batch)
-        return (np.array(states), np.array(actions), np.array(rewards), 
-                np.array(next_states), np.array(dones))
-    
-    def __len__(self):
-        return len(self.buffer)
+from utils import ReplayBuffer, reasonable_actions
 
 class DQNAgent:
     """DQN Agent for traffic signal control"""
@@ -113,10 +68,10 @@ class DQNAgent:
         self.target_net.eval()
         
         # Optimizer and loss
-        self.optimizer = optim.Adam(self.policy_net.parameters(), 
+        self.optimizer = torch.optim.Adam(self.policy_net.parameters(), 
                                     lr=self.config['learning_rate'])
         self.criterion = torch.nn.MSELoss()
-        # self.scheduler = optim.lr_scheduler.StepLR(self.optimizer, step_size=1000, gamma=0.9)
+        # self.scheduler = torch.optim.lr_scheduler.StepLR(self.optimizer, step_size=1000, gamma=0.9)
         
         # Replay buffer
         self.memory = ReplayBuffer(self.config['buffer_size'])
@@ -510,6 +465,7 @@ class DQNAgent:
 
 # Example usage
 if __name__ == "__main__":
+    import argparse
 
     parser = argparse.ArgumentParser()
     parser.add_argument("-f", "--file", type=str, default="map_2", help="SUMO file to use")
@@ -586,19 +542,4 @@ if __name__ == "__main__":
 
     num_episodes = training_config['num_episodes']
 
-    agent.run(num_episodes)
-    
-# ==== HOW TO RUN =====
-'''bash
-pip install wandb
-wandb login
-
-To use log your test:
-python environment/NN_wandb.py --wandb-name <testname>
- 
-If you dont want to use wandb:
-python environment/NN_wandb.py --no-wandb
-'''    
-
-
-    
+    agent.run(num_episodes)    
